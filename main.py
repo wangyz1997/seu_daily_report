24a386836d472c93407a430a7076e38b61ee8b25
wangyz <798475117@qq.com>
2020-09-18 23:13:35 +0800

    增加输入体温功能 加入设置位置功能（无效）

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from config import username, password
 import os
-import time
+import random
 
 driver = webdriver.Chrome(executable_path="Chromedriver.exe")
 login_url = 'http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/*default/index.do#/dailyReport'
+
+location = '江苏省, 南京市, 玄武区'
+temp_range = (35.5, 36.5)
 
 """
 使用方法请参见README.md
@@ -27,14 +30,31 @@ def press_add_btn():
     """点击填报按钮"""
     btn_found = False
     buttons = driver.find_elements_by_css_selector('.mint-button--normal')  # 根据CSS找到所有按钮
-    for btn in buttons:
-        if btn.get_attribute('textContent').find('新增') >= 0:  # 找到按钮文字是"新增"的按钮
+    for add_btn in buttons:
+        if add_btn.get_attribute('textContent').find('新增') >= 0:  # 找到按钮文字是"新增"的按钮
             btn_found = True
-            btn.click()  # 点击新增填报按钮
+            add_btn.click()  # 点击新增填报按钮
             break
 
     if not btn_found:
         raise Exception('今日已填报')
+
+
+def input_data():
+    """输入体温及定位"""
+    inputs = driver.find_elements_by_class_name('mint-field-core')
+    for temp_input in inputs:
+        if temp_input.get_attribute("placeholder").find("请输入当天晨检体温") >= 0:
+            temp_input.click()  # 点击输入框
+            temp = random.randint(int(temp_range[0]*10), int(temp_range[1]*10))  # 产生随机体温
+            temp_input.send_keys(str(temp/10))  # 输入体温
+
+    cells = driver.find_elements_by_class_name('emapm-item')  # 找到所有列表项
+    for location_cell in cells:
+        cell_title = location_cell.find_element_by_class_name('mint-cell-title')
+        if cell_title.get_attribute('textContent') == '当前位置':  # 找到当前位置列表项
+            location_input = location_cell.find_element_by_class_name('mint-cell-value')  # 找到输入框
+            location_input.set_attribute('textContent', location)  # 强制设置位置
 
 
 if __name__ == '__main__':
@@ -42,14 +62,16 @@ if __name__ == '__main__':
         # 登录
         login()
         # 网站响应较慢 需要延时
-        time.sleep(5)
+        driver.implicitly_wait(5)
         # 点击填报按钮
         press_add_btn()
+        # 输入数据
+        input_data()
         # 暂停
         os.system('pause')
 
-    except Exception as e:
-        print(e)
+    # except Exception as e:
+        # print(e)
 
     finally:
         # driver.quit()  # 退出整个浏览器
