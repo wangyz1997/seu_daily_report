5e826c81b216f880c63626a92c2a51154e394f3f
wangyz <798475117@qq.com>
2020-09-20 00:33:12 +0800

    优化程序结构

--- main.py
+++ main.py
@@ -17,16 +17,20 @@ system_type = platform.system()
 if system_type == 'Windows':
     driver = webdriver.Chrome(executable_path=os.path.join(driver_folder, "Chromedriver.exe"))
 else:
-    driver = webdriver.Chrome(executable_path=os.path.join(driver_folder, "Chromedriver"))
+    driver = webdriver.Chrome(executable_path=os.path.join(driver_folder, "chromedriver"))
 
 daily_report_url = 'http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/*default/index.do#/dailyReport'
 campus_apply_url = 'http://ehall.seu.edu.cn/qljfwapp3/sys/lwWiseduElectronicPass/*default/index.do'
 
-def find_element_by_class_keyword(drv, class_name, keyword):
-    elements = WebDriverWait(drv, 10).until(
-        lambda d: d.find_elements_by_class_name(class_name))
+
+def find_element_by_xpath_wait(drv, xpath, timeout):
+    return WebDriverWait(drv, timeout).until(lambda d: d.find_element_by_xpath(xpath))
+
+
+def find_element_by_class_keyword(drv, class_name, attribute, keyword):
+    elements = drv.find_elements_by_class_name(class_name)
     for element in elements:
-        if element.text.find(keyword) >= 0:
+        if element.get_attribute(attribute).find(keyword) >= 0:
             return element
 
     return None
@@ -36,7 +40,7 @@ def login(drv, cfg):
     """登录"""
     username_input = drv.find_element_by_id('username')
     password_input = drv.find_element_by_id('password')
-    login_button = drv.find_element_by_class_name('auth_login_btn')  # 登录按钮
+    login_button = find_element_by_class_keyword(drv, 'auth_login_btn', 'textContent', '登录')  # 登录按钮
 
     username_input.send_keys(cfg.username)
     password_input.send_keys(cfg.password)
@@ -45,24 +49,26 @@ def login(drv, cfg):
 
 def daily_report(drv, cfg):
     """进行每日上报"""
-
     # 新增填报
-    add_btn = find_element_by_class_keyword(drv, 'mint-button--normal', '新增')  # 找到按钮文字是"新增"的按钮
-    if add_btn:
-        add_btn.click()  # 点击新增填报按钮
-    else:
+    add_btn = find_element_by_xpath_wait(drv, '//*[@id="app"]/div/div[1]/button[1]', 10)  # 找到按钮文字是"新增"的按钮
+    if add_btn.text == '退出':
         print('今日已填报')
         return
+    else:
+        add_btn.click()  # 点击新增填报按钮
+        drv.implicitly_wait(3)  # 等待界面动画
 
     # 输入体温
-    temp_input = find_element_by_class_keyword(drv, 'mint-field-core', '请输入当天晨检体温')
+    temp_input = find_element_by_class_keyword(drv, 'mint-field-core', 'placeholder', '请输入当天晨检体温')
     temp_input.click()  # 点击输入框
     temp = random.randint(int(cfg.temp_range[0]*10), int(cfg.temp_range[1]*10))  # 产生随机体温
     temp_input.send_keys(str(temp/10))  # 输入体温
 
     # 点击提交按钮并确认
-    find_element_by_class_keyword(drv, 'mint-button--large', '确认并提交').click()  # 点击提交按钮
-    find_element_by_class_keyword(drv, 'mint-msgbox-confirm', '确定').click()  # 点击确认按钮
+    find_element_by_class_keyword(drv, 'mint-button--large', 'textContent', '确认并提交').click()  # 点击提交按钮
+    find_element_by_class_keyword(drv, 'mint-msgbox-confirm', 'textContent', '确定').click()  # 点击确认按钮
+
+    print('每日疫情上报成功!')
 
 
 if __name__ == '__main__':
@@ -73,11 +79,8 @@ if __name__ == '__main__':
         login(driver, config)
         # 每日填报
         daily_report(driver, config)
-        print('每日疫情上报成功!')
         # 打开入校申请网站
         driver.get(campus_apply_url)
-
-
 
     finally:
         driver.quit()  # 退出整个浏览器
