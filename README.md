43f606a435606d22f3a65b0b050878bb90b2d788
Chentingz <ct2ing@gmail.com>
2021-03-28 17:32:14 +0800

    Support Firefox and Email Notifications

--- README.md
+++ README.md
@@ -1,37 +1,24 @@
-# [东南大学](https://www.seu.edu.cn) 每日健康上报及入校申请自动化脚本
+# [东南大学](https://www.seu.edu.cn) 疫情每日上报及入校申请自动化脚本
 
 > 免责声明：本脚本仅为个人为学习python之目的所编写，使用该脚本造成的一切后果均由使用者承担。
 >
 > 本人仍然提倡每日按时手动进行疫情上报与入校申请，配合学校进行好疫情防控。
 
-这是一个每日健康上报及入校申请自动化脚本，通过正确配置之后，可以实现每日自动健康上报和入校申请，并通过微信或邮件推送上报结果。
+# 使用方法：
 
-# 一、依赖
-## 1. WebDriver
-**如果你用的浏览器是[Google Chrome](https://www.google.cn/chrome/) ，那么**
+## 1. 下载WebDriver
+推荐使用 [Google Chrome](https://www.google.cn/chrome/) 作为本脚本使用的浏览器，本文档后面也将以Google Chrome作为示范。
 
-1. 请检查安装的Chrome版本：浏览器右上角的3个点-帮助-关于Google Chrome。
+首先请检查您安装的Chrome版本：浏览器右上角的3个点-帮助-关于Google Chrome。
 
-2. 进入 [淘宝Chrome Driver镜像站](http://npm.taobao.org/mirrors/chromedriver/) 下载与浏览器相同版本的Chrome Driver。
+接下来，进入 [淘宝Chrome Driver镜像站](http://npm.taobao.org/mirrors/chromedriver/) 下载与您使用浏览器相同版本的Chrome Driver。
 
-3. 下载后与本脚本文件放置于同一目录中,Windows平台命名为`chromedriver.exe`。
+下载后请与本脚本文件放置于同一目录中, Windows平台命名为`Chromedriver.exe`。
 
-   Linux / macOS平台请将可执行文件放置于与脚本相同的目录中，并自行修改脚本中`executable_path`中的文件名。
+Linux/macOS平台请将可执行文件放置于与脚本相同的目录中，并自行修改脚本中`executable_path`中的文件名。
 
-**如果你用的浏览器是 [Mozilla Firefox](https://www.firefox.com ) ，那么**
-
-1. 请检查安装的Firefox版本：浏览器右上角3条杠-帮助-关于Firefox。
-
-2. 进入 [淘宝geckodriver镜像站 ](http://npm.taobao.org/mirrors/geckodriver/)下载与浏览器相同版本的geckodriver。
-
-3. 下载后与本脚本文件放置于同一目录中, Windows平台命名为`geckodriver.exe`。
-
-   Linux / macOS平台请将可执行文件放置于与脚本相同的目录中，并自行修改脚本中`executable_path`中的文件名。
-
-> 提示：本脚本目录中已经放置`chromedriver_90.0.4430.24`和`geckodriver_v0.29.0`，若运行有问题，请下载与浏览器相同版本的WebDriver进行替换。
-
-## 2. `Python`依赖
-本脚本依赖`selenium`包与`requests`包，使用`pip`进行安装。
+## 2. 下载`python`依赖
+本脚本依赖`selenium`包与`requests`包。要安装它们，只需要使用`pip`即可。
 
 ```shell script
 # Windows
@@ -40,143 +27,38 @@ pip install requests selenium -i https://pypi.douban.com/simple --user
 pip3 install requests selenium -i https://pypi.douban.com/simple --user
 ```
 
-
-# 二、使用方法
-## 1. 配置脚本
-
-1. 将脚本目录中的`config_sample.json`重命名为`config.json`。
-
-2. 打开`config.json`，根据需求填写相关字段，字段描述如下。
-   | 字段名                      | 描述                                      | 是否必填 |
-   | --------------------------- | ----------------------------------------- | -------- |
-   | `username`                  | 一卡通账号（9位）                         | YES      |
-   | `password`                  | 一卡通密码                                | YES      |
-   | `temp_range`                | 体温范围                                  | YES      |
-   | `places`                    | 申请入校区域                              | NO       |
-   | `reasons`                   | 申请入校理由                              | NO       |
-   | `server_chan_key`           | Server酱SCKEY                             | NO       |
-   | `from_addr`                 | 发送执行结果的邮箱地址（建议使用SEU邮箱） | NO       |
-   | `email_password`            | 发送邮箱密码                              | NO       |
-   | `smtp_server`               | 发送邮箱的SMTP服务器地址                  | NO       |
-   | `to_adr`                    | 接收执行结果的邮箱地址                    | NO       |
-   | `enable_enter_campus_apply` | 开启/关闭入校申请功能（默认关闭）         | YES      |
-   | `browser`                   | 选择浏览器类型（chrome / firefox）        | YES      |
-
-   向其中的`username`与`password`填入您的一卡通账号与密码。
-
-   在`temp_range`中可自定义您想要填写的体温范围。 ***请一定要在确定自己体温正常的情况下使用此功能。***
-   
-   在`places`与`reasons`中还可以自定义您每日想要填写的入校区域与入校理由，其中的第一个元素为周一，最后一个元素为周日。`reasons`的取值`0-7`所对应的理由如下所示。
-   
-   |          理由          | 对应数字 |
-   | :--------------------: | :------: |
-   |      到教学楼上课      |    0     |
-   |      实验室做实验      |    1     |
-   |      到办公室科研      |    2     |
-   |    到图书馆学习借书    |    3     |
-   | 到职能部门、院系办手续 |    4     |
-   |          开会          |    5     |
-   |    往返无线谷实验室    |    6     |
-   |          其他          |    7     |
-   
-   `server_chan_key`用于配置Server酱推送功能。
-   
-   `from_addr`、`email_password`、`smtp_server`、`to_addr`用于配置邮件推送功能。
-   
-   除此之外，通过将`enable_enter_campus_apply`设置为`true`来启动入校申请或`false`来关闭入校申请。
-   
-   `browser`用于设置使用的浏览器，可填写`chrome`或`firefox`
-
-### 配置实现多个用户上报
-
-若想要同时为多个用户进行健康上报或入校申请，只需要将`config.json`中`users`字段的配置信息复制多份，每一份均填写一位用户的信息即可。
-
-例如：
-
-```
-"users": [
-      {
-         "username": "xxxxxx", "password": "***",
-         "temp_range": [35.5, 36.2],
-         "places": [],
-         "reasons": [],
-         "server_chan_key": "",
-         "from_addr": "",
-         "email_password": "",
-         "smtp_server": "",
-         "to_addr": ""
-      },
-        {
-         "username": "xxxxxx", "password": "***",
-         "temp_range": [35.1, 36.8],
-         "places": [],
-         "reasons": [],
-         "server_chan_key": "",
-         "from_addr": "",
-         "email_password": "",
-         "smtp_server": "",
-         "to_addr": ""
-      }
-   ]
-```
-
-### 使用 Server酱 推送上报结果
-
-[Server酱](http://sc.ftqq.com/) 是一个微信推送工具，可以将服务器端执行结果推送到您的微信上。
+## 3. 使用 [server酱](http://sc.ftqq.com/) 接收脚本执行结果
+[server酱](http://sc.ftqq.com/) 是一个微信推送工具，可以将服务器端执行结果推送到您的微信上。
 
 本脚本支持 server酱 推送，您只需要按照其网站上的指引，使用`GitHub`账号登录并扫码绑定您的微信，即可获得`SCKEY`。
 将取得的`SCKEY`填入`config.json`中的`server_chan_key`字段中，即可启用微信推送功能。
 
 > 提示：将`server_chan_key`留空，即可禁用微信推送功能。
 
-### 使用 邮件 推送上报结果
+## 4. 配置脚本
 
-本脚本支持邮件推送，您只需要在`config.json`文件中填写`from_addr`、`email_password`、`smtp_server`、`to_addr`字段。即可启用邮件推送功能。
+> 提示：与`1.0`版本中不同，`2.0`中为了实现多用户功能，改用了`json`格式的配置文件，如果您进行了版本升级，请重新进行配置。
 
-例如：填写字段如下，则会使用SEU邮箱将脚本执行结果推送至指定邮箱
+将脚本目录中的`config_sample.json`重命名为`config.json`。
 
-| 字段名           | 值              | 描述                   |
-| ---------------- | --------------- | ---------------------- |
-| `from_addr`      | xxx@seu.edu.cn  | 发送执行结果的邮箱地址 |
-| `email_password` | ***             | 发送执行结果的邮箱密码 |
-| `smtp_server`    | smtp@seu.edu.cn | 发送邮箱的SMTP服务器   |
-| `to_addr`        | xxx@example.com | 接收邮箱地址           |
+打开`config.json`，向其中的`username`与`password`后填入您的一卡通账号与密码。
 
-> 提示：将`from_addr`、`email_password`、`smtp_server`、`to_addr`留空，即可禁用邮件推送功能。
+您还可以在其中的`temp_range`中自定义您想要填写的体温范围。 ***请一定要在确定自己体温正常的情况下使用此功能。***
 
-## 2. 运行脚本
-在正式运行脚本之前，请确认脚本目录下的文件和下面相同：
+## 5. 运行脚本
+在您正式运行脚本之前，请确认您脚本目录下的文件和下面相同：
 
 ```
 |-- seu_daily_report
     |-- .gitignore
-    |-- autorun.bat
     |-- chromedriver.exe
-    |-- geckodriver.exe
-    |-- config_sample.json
+    |-- config.json
     |-- main.py
     |-- README.md
-    |-- img
 ```
 
-之后，使用`python`运行`main.py`或直接运行`auto.bat`（Windows平台下）即可。
-您也可以将脚本与运行环境部署到云服务器上，并设置定时计划任务，实现每日自动上报。
+之后，使用`python`运行`main.py`即可。
+您也可以将脚本与运行环境部署到云服务器上，并设置定时计划任务，实现每日自动签到。
 
-下面介绍如何在Windows上设置实现每日自动上报。
-
-1. `Ctrl + R`输入`taskschd.msc`，打开任务计划程序
-
-2. 创建任务，在常规中名称填写`SEU Daily Reporter`，勾选【不管任务是否登录都要运行】、【不存储密码】、【使用最高权限运行】。
-
-   ![创建任务](./img/创建任务.png)
-
-3. 新建触发器，设置为【每天】执行，设置【计划开始的时间】和【随机延迟时间】，点击确定。
-
-   ![新建触发器](./img/新建触发器.png)
-
-4. 新建操作，填写【运行脚本所在的绝对路径】和【脚本所在目录】，点击确定。
-
-   ![新建操作](./img/新建操作.png)
-
-5. 点击【确定】完成计划任务创建，即可实现每日自动上报。
-
+## 6. 进阶
+若想要同时为多个用户执行本脚本，只需要将`config.json`中`users`字段的配置信息复制多份，每一份均填写一位用户的信息即可。
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-#-*- coding:utf-8 –*-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import os
 import json
 import random
 import time
-import datetime
 import traceback
 import requests
 import smtplib
@@ -14,36 +12,40 @@ from email.header import Header
 from email.utils import parseaddr, formataddr
 
 
-date_of_today = datetime.datetime.now()  # 当日日期
-date_of_tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)  # 次日日期
 current_folder = os.path.split(os.path.realpath(__file__))[0]  # 当前py文件路径
-# 疫情每日上报和入校申请URL
+# 疫情每日打卡URL
 daily_report_url = 'http://ehall.seu.edu.cn/qljfwapp2/sys/lwReportEpidemicSeu/*default/index.do#/dailyReport'
-enter_campus_apply_url = 'http://ehall.seu.edu.cn/qljfwapp3/sys/lwWiseduElectronicPass/*default/index.do'
 server_chan_url = 'http://sc.ftqq.com/{}.send/'
 
-def email_send(from_addr, email_password, smtp_server, to_addr, message):
-    """发送上报结果至指定邮箱.
+def email_send(username, password, remote_email_addr, message):
+    """
+    用SEU邮箱发送上报结果至指定邮箱.
 
     Args:
-        msg: str.
+        username: 一卡通账号
+        password: 一卡通密码
+        remote_email_addr: 指定邮箱地址
+        msg: 上报结果.
     """
-    if len(from_addr) <=0 or len(email_password) <=0 or len(smtp_server) <=0 or len(to_addr) <=0:
+    if len(remote_email_addr) <=0 :
         return None
 
+    seu_email_addr = str(username) +"@seu.edu.cn"
+
     msg = MIMEText(message, 'plain', 'utf-8')
-    msg['From'] = format_addr("SEU Daily Reporter {}".format(from_addr))
-    msg['To'] = format_addr("Admin {}".format(to_addr))
+    msg['From'] = format_addr("SEU Daily Reporter {}".format(seu_email_addr))
+    msg['To'] = format_addr("Admin {}".format(remote_email_addr))
     msg['Subject'] = Header("每日上报结果", "utf-8").encode()
 
-    server = smtplib.SMTP_SSL(smtp_server, 465)  # 启用SSL发信, 端口一般是465
-    server.login(from_addr, email_password)
-    server.sendmail(from_addr, [to_addr], msg.as_string())
+    server = smtplib.SMTP_SSL("smtp.seu.edu.cn", 465)  # 启用SSL发信, 端口一般是465
+    server.login(seu_email_addr, password)
+    server.sendmail(seu_email_addr, [remote_email_addr], msg.as_string())
     server.quit()
 
 
 def format_addr(s):
-    """格式化地址.
+    """
+    格式化地址.
 
     Args:
         s: str, 类似"SEU Daily Reporter <xxx@seu.edu.cn>"
@@ -52,8 +54,8 @@ def format_addr(s):
         str, 类似"'=?utf-8?q?SEU Daily Reporter?= <xxx@seu.edu.cn>'"
     """
     name, addr = parseaddr(s)
-
     return formataddr((Header(name, 'utf-8').encode(), addr))
+
 
 def server_chan_send(key, content, description):
     print(content, '\r\n', description)
@@ -92,99 +94,6 @@ def find_element_by_class_keyword(drv, class_name, keyword):
     return None
 
 
-def select_default_item_by_keyword(drv, keyword):
-    """在入校申请时选择默认项"""
-    items = drv.find_elements_by_class_name('emapm-item')  # 找到所有项目
-    for item in items:
-        if item.text.find(keyword) >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直到元素可见
-            item.click()
-
-    wait_element_by_class_name(drv, 'mint-picker__confirm', 5)  # 等待弹出动画
-    time.sleep(1)
-    find_element_by_class_keyword(drv, 'mint-picker__confirm', '确定').click()  # 点击确定
-    time.sleep(1)
-
-
-def select_default_item_in_areas(drv, keyword):
-    """在入校申请时选择通行区域"""
-    items = drv.find_elements_by_class_name('emapm-item')  # 找到所有项目
-    for item in items:
-        if item.text.find(keyword) >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直元素可见
-            item.click()
-
-    wait_element_by_class_name(drv, 'mint-checkbox-new-row', 5)  # 等待弹出动画
-    time.sleep(1)
-    drv.find_element_by_class_name('mint-checkbox-new-row').click()  # 点击复选框
-    time.sleep(1)
-    find_element_by_class_keyword(drv, 'mint-selected-footer-confirm', '确定').click()  # 点击确定按钮
-    time.sleep(1)
-
-
-def picker_click(drv, column, cnt):
-    """选择滚轮的中的项目"""
-    pickers = column.find_elements_by_class_name('mt-picker-column-item')  # 所有滚动元素
-    drv.execute_script("arguments[0].scrollIntoView();", pickers[cnt])  # 滚动页面直元素可见
-    pickers[cnt].click()  # 选中元素
-
-
-def time_date_reason_pick(drv, cfg):
-    """选择通行时间及申请理由"""
-    items = drv.find_elements_by_class_name('emapm-item')  # 找到所有项目
-    for item in items:
-        if item.text.find('通行开始时间') >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直元素可见
-            item.click()  # 点击项目
-            columns = item.find_elements_by_class_name('mint-picker-column')  # 找到项目内所有滚轮
-            time.sleep(1)
-            picker_click(drv, columns[0], date_of_tomorrow.date().year - 1920)  # 年 从1920年开始
-            picker_click(drv, columns[1], date_of_tomorrow.date().month - 1)  # 月 从1开始
-            picker_click(drv, columns[2], date_of_tomorrow.date().day - 1)  # 日 从1开始
-            picker_click(drv, columns[3], 7)  # 时
-            picker_click(drv, columns[4], 31)  # 分 入校时间为7时31分
-            time.sleep(1)
-            find_element_by_class_keyword(drv, 'mint-picker__confirm', '确定').click()  # 点击确定按钮
-            time.sleep(1)
-
-        if item.text.find('通行结束时间') >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直元素可见
-            item.click()  # 点击项目
-            columns = item.find_elements_by_class_name('mint-picker-column')  # 找到项目内所有滚轮
-            time.sleep(1)
-            picker_click(drv, columns[0], date_of_tomorrow.date().year - 1920)  # 年 从1920年开始
-            picker_click(drv, columns[1], date_of_tomorrow.date().month - 1)  # 月 从1开始
-            picker_click(drv, columns[2], date_of_tomorrow.date().day - 1)  # 日 从1开始
-            picker_click(drv, columns[3], 21)  # 时
-            picker_click(drv, columns[4], 59)  # 分 出校时间为21时59分
-            time.sleep(1)
-            find_element_by_class_keyword(drv, 'mint-picker__confirm', '确定').click()  # 点击确定按钮
-            time.sleep(1)
-
-        if item.text.find('申请理由') >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直元素可见
-            item.click()  # 点击项目
-            column = item.find_element_by_class_name('mint-picker-column')  # 找到项目内所有滚轮
-            time.sleep(1)
-            picker_click(drv, column, cfg['reasons'][date_of_tomorrow.date().weekday()])  # 根据星期自动填写目的
-            time.sleep(1)
-            find_element_by_class_keyword(drv, 'mint-picker__confirm', '确定').click()  # 点击确定按钮
-            time.sleep(1)
-
-
-def check_todays_report(drv):
-    """检查当日是否已进行过入校申请"""
-    items = drv.find_elements_by_class_name('res-list')  # 找到所有已填报项目
-    latest = find_element_by_class_keyword(items[0], 'res-item-ele', '申请时间').text  # 第一个项目即为最近一次的填报
-    latest = latest[latest.find(' ') + 1: latest.rfind(' ')]  # 只保留日期
-    latest_date = datetime.datetime.strptime(latest, '%Y-%m-%d').date()  # 转换
-
-    if latest_date == date_of_today.date():  # 今日已经填报过了
-        return True
-
-    return False
-
-
 def login(drv, cfg):
     """登录"""
     username_input = drv.find_element_by_id('username')  # 账户输入框
@@ -204,14 +113,13 @@ def daily_report(drv, cfg):
     wait_element_by_class_name(drv, 'mint-loadmore-top', 30)  # 等待界面加载 超时30s
     time.sleep(1)
     add_btn = drv.find_element_by_xpath('//*[@id="app"]/div/div[1]/button[1]')  # 找到新增按钮
-    if add_btn.text == '退出':
-        result_msg = str(cfg['username']) + ' 今日已经进行过健康申报！'
-        server_chan_send(cfg['server_chan_key'], result_msg, '')
-        email_send(cfg['from_addr'], cfg['email_password'], cfg['smtp_server'], cfg['to_addr'], result_msg)
+    if add_btn.text == '退出':  # 没有找到新增按钮
+        server_chan_send(cfg['server_chan_key'], '今日已经进行过疫情上报！', '')
+        email_send(cfg['username'], cfg['password'], cfg['email_addr'], '今日已经进行过疫情上报！')
         return
     else:
         add_btn.click()  # 点击新增填报按钮
-        time.sleep(3)  # 等待界面动画
+        time.sleep(10)  # 等待界面动画
 
     # 输入体温
     temp_input = find_element_by_class_placeholder_keyword(drv, 'mint-field-core', '请输入当天晨检体温')
@@ -227,82 +135,28 @@ def daily_report(drv, cfg):
     time.sleep(1)
     find_element_by_class_keyword(drv, 'mint-msgbox-confirm', '确定').click()  # 点击确认按钮
 
-    result_msg =  str(cfg['username']) + ' 每日健康申报成功!\n' + "今日体温填报：" + str(temp / 10) + "℃"
-    server_chan_send(cfg['server_chan_key'], result_msg, '')
-    email_send(cfg['from_addr'], cfg['email_password'], cfg['smtp_server'], cfg['to_addr'], result_msg)
+    server_chan_send(cfg['server_chan_key'], str(cfg['username'])+'每日疫情上报成功!', '')
+    email_send(cfg['username'], cfg['password'], cfg['email_addr'], '每日疫情上报成功!')
 
 
-def enter_campus_apply(drv, cfg):
-    """进行入校申请"""
-    wait_element_by_class_name(drv, 'res-item-ele', 30)  # 等待界面加载 超时30s
-
-    if check_todays_report(drv):  # 今日已进行入校申请
-        result_msg = str(cfg['username']) + ' 今日已经进行过入校申请！'
-        server_chan_send(cfg['server_chan_key'], result_msg, '')
-        email_send(cfg['from_addr'], cfg['email_password'], cfg['smtp_server'], cfg['to_addr'], result_msg)
-        return
-
-    drv.find_element_by_xpath('//*[@id="app"]/div/div[3]').click()  # 找到新增按钮
-
-    time.sleep(2)  # 等待窗口动画弹出
-    popup = find_element_by_class_keyword(drv, 'mint-msgbox-confirm', '确定')  # 查询是否弹出了对话框
-    if popup is not None:  # 如果弹出了对话框
-        result_msg = str(cfg['username']) + ' 当前不在入校申请填报时间!'
-        server_chan_send(cfg['server_chan_key'], result_msg, '')
-        email_send(cfg['from_addr'], cfg['email_password'], cfg['smtp_server'], cfg['to_addr'], result_msg)
-        return
-
-    wait_element_by_class_name(drv, 'emapm-item', 30)  # 等待界面加载
-    select_default_item_by_keyword(drv, '身份证件类型')
-    select_default_item_by_keyword(drv, '工作场所是否符合防护要求')
-    select_default_item_by_keyword(drv, '工作人员能否做好个人防护')
-    select_default_item_by_keyword(drv, '是否已在南京居家隔离')
-    select_default_item_by_keyword(drv, '目前身体是否健康')
-
-    select_default_item_in_areas(drv, '通行区域')  # 填写通行区域
-
-    time_date_reason_pick(drv, cfg)  # 填入入校时间/出校时间/入校理由
-
-    temp_input = find_element_by_class_placeholder_keyword(drv, 'mint-field-core', '请输入所到楼宇')
-    drv.execute_script("arguments[0].scrollIntoView();", temp_input)  # 滚动页面直元素可见
-    temp_input.click()  # 点击输入框
-    temp_input.send_keys(cfg['places'][date_of_tomorrow.weekday()])  # 输入入校地址
-
-    find_element_by_class_keyword(drv, 'tg-button', '提交').click()  # 点击提交按钮
-    wait_element_by_class_name(drv, 'mint-msgbox-confirm', 5)  # 等待弹出动画
-    time.sleep(1)
-    find_element_by_class_keyword(drv, 'mint-msgbox-confirm', '确定').click()  # 点击确认按钮
-
-    result_msg = str(cfg['username'])+' 每日入校申请成功!'
-    server_chan_send(cfg['server_chan_key'], result_msg, '')
-    email_send(cfg['from_addr'], cfg['email_password'], cfg['smtp_server'], cfg['to_addr'],  result_msg)
-
-
-def run(profile, config):
-    if config["browser"] == "chrome":
-        driver = webdriver.Chrome(executable_path=os.path.join(current_folder, "chromedriver.exe"))
-    elif config["browser"] == "firefox":
+def run(profile, cfg):
+    if cfg['browser'] == "chrome":
+        driver = webdriver.Chrome(executable_path=os.path.join(current_folder, "Chromedriver.exe"))
+    elif cfg['browser'] == "firefox":
         driver = webdriver.Firefox(executable_path=os.path.join(current_folder, "geckodriver.exe"))
     try:
         # 打开疫情填报网站
         driver.get(daily_report_url)
         # 登录
         login(driver, profile)
-        # 每日填报
+        # 每日打卡
         daily_report(driver, profile)
-        # 打开入校申请网站
-        if config["enable_enter_campus_apply"]:
-            time.sleep(5)
-            driver.get(enter_campus_apply_url)
-            # 填写入校申请
-            enter_campus_apply(driver, profile)
     except Exception:
         exception = traceback.format_exc()
-        result_msg = profile['username'] + ' 出错啦,请尝试手动重新填报'
-        server_chan_send(profile['server_chan_key'], result_msg, exception)
-        email_send(cfg['from_addr'], cfg['email_password'], cfg['smtp_server'], cfg['to_addr'],result_msg)
+        server_chan_send(profile['server_chan_key'], '出错啦,请尝试手动重新填报', exception)
+        email_send(cfg['username'], cfg['password'], cfg['email_addr'], '出错啦,请尝试手动重新填报')
     finally:
-        time.sleep(3)
+        time.sleep(1)
         driver.quit()  # 退出整个浏览器
 
 
