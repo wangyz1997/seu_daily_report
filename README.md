81784a75b1c814c106b5ac6039b9461e43340fee
wangyz1997 <wangyz1997@126.com>
2022-11-19 17:19:10 +0800

    移除过时代码

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
-# [东南大学](https://www.seu.edu.cn) 疫情每日上报自动化脚本
+# 东南大学疫情每日上报自动化脚本
 
 > 免责声明：本脚本仅为个人为学习python之目的所编写，使用该脚本造成的一切后果均由使用者承担。
 >
 > 本人仍然提倡每日按时手动进行疫情上报与入校申请，配合学校进行好疫情防控。
 
-这是一个每日健康上报的自动化脚本，通过正确配置之后，可以实现每日自动健康上报，并通过微信或邮件推送上报结果。
-
-> ⚠ 仓库中最新的代码已经恢复了入校申请的功能，但由于上传通信行程码的功能使用了微信内置浏览器API，普通浏览器暂时无法模拟。
-> 若您能够解决此问题，欢迎提出PR。同时，该功能处于默认*关闭*状态。
+这是一个每日健康上报的自动化脚本，可以实现每日自动健康上报，并通过微信或邮件推送上报结果。
 
 # 使用方法：
 
@@ -15,21 +12,21 @@
 
 **如果你用的浏览器是 [Google Chrome](https://www.google.cn/chrome/) ，那么**
 
-1. 首先请检查您安装的Chrome版本：浏览器右上角的3个点-帮助-关于Google Chrome。
+1. 请检查您安装的Chrome版本：浏览器右上角的3个点-帮助-关于Google Chrome。
 
-2. 进入 [淘宝Chrome Driver镜像站](http://npm.taobao.org/mirrors/chromedriver/) 下载与您使用浏览器相同版本的Chrome Driver。
+2. 进入 [淘宝Chrome Driver镜像站](http://npm.taobao.org/mirrors/chromedriver/) 下载与您使用浏览器相同版本的`chromedriver`。
 
-3. 下载后请与本脚本文件放置于同一目录中, Windows平台命名为`Chromedriver.exe`。
+3. 下载后请与本脚本文件放置于同一目录中。
 
 **如果你用的浏览器是 [Mozilla Firefox](https://www.firefox.com) ，那么**
 
 1. 请检查安装的Firefox版本：浏览器右上角3条杠-帮助-关于Firefox。
 
-2. 进入 [淘宝geckodriver镜像站](http://npm.taobao.org/mirrors/geckodriver/) 下载与浏览器相同版本的geckodriver。
+2. 进入 [淘宝geckodriver镜像站](http://npm.taobao.org/mirrors/geckodriver/) 下载与浏览器相同版本的`geckodriver`。
 
-3. 下载后与本脚本文件放置于同一目录中, Windows平台命名为`geckodriver.exe`。
+3. 下载后与本脚本文件放置于同一目录中。
 
-> 无需更改`WebDriver`的文件名，程序会自动识别各个平台对应的文件名。
+> 无需更改WebDriver的文件名，程序会自动识别各个平台对应的文件名。如同时存在chromedriver与geckodriver，将优先使用`chromedriver`。
 
 ## 2. 安装`python`依赖
 
@@ -44,12 +41,11 @@ pip3 install requests selenium -i https://pypi.douban.com/simple --user
 
 ## 3. 配置脚本
 
-1. 将脚本目录中的`config_sample.json`重命名为`config.json`。
+1. 将脚本目录中的`config_sample.json`复制并重命名为`config.json`。
 
 2. 打开`config.json`，向各个字段中填入合适的值，每个字段的描述如下表所示。
 
 3. [server酱](http://sc.ftqq.com/) 是一个微信推送工具，可以将服务器端执行结果推送到您的微信上。本脚本支持`server酱`推送，您只需要按照其网站上的指引，使用`GitHub`账号登录并扫码绑定您的微信，即可获得`SCKEY`。将取得的`SCKEY`填入`config.json`中的`server_chan_key`字段中，即可启用微信推送功能。
-> 由于微信模板消息即将停用，`server酱`也将会停止微信推送服务。若需要使用`server酱`官方提供的其他推送通道，可以直接在`server酱`官网绑定新的推送通道，并将新的`SCKEY`填入。本程序中的接口无需更新。
 
 4. 本脚本同样支持使用邮箱推送执行结果。在`config.json`中的`email_addr`字段填写接收邮件使用的邮箱，即可使用打卡所使用账号的东大邮箱发送一封邮件到该地址。
 
@@ -60,29 +56,10 @@ pip3 install requests selenium -i https://pypi.douban.com/simple --user
 | `temp_range`                | 体温范围 ***请一定要在确定自己体温正常的情况下使用此功能*** |
 | `server_chan_key`           | Server酱SCKEY（留空不启用）                           |
 | `email_addr`                | 接收执行结果的邮箱地址（留空不启用）                     |
-| `places`                    | 入校申请时周一到周日的入校区域                          |
-| `reasons`                   | 入校申请时周一到周日的入校原因                          |
-| `browser`                   | 选择浏览器类型（chrome / firefox）                    |
-| `enter_campus_apply`        | 选择是否开启入校申请功能                               |
-
-5. 在`places`与`reasons`中您还可以自定义您每日想要填写的入校区域与入校理由，其中的第一个元素为周一，最后一个元素为周日。
-
-`reasons`的取值`0-7`所对应的理由如下所示。
-
-|          理由         | 对应数字 |
-| :------------------: | :-----: |
-|      到教学楼上课      |    0    |
-|      实验室做实验      |    1    |
-|      到办公室科研      |    2    |
-|     到图书馆学习借书    |    3    |
-|  到职能部门、院系办手续  |    4    |
-|          开会         |    5    |
-|     往返无线谷实验室    |    6    |
-|          其他         |    7    |
 
 ## 4. 运行脚本
 
-在您正式运行脚本之前，请确认您脚本目录下存在`main.py` `config.json`与`chromedriver.exe`或`geckodriver.exe`文件。
+在您正式运行脚本之前，请确认您脚本目录下存在`main.py` `config.json`与`chromedriver`或`geckodriver`文件。
 
 然后，使用`python`/`python3`运行`main.py`即可启动单次填报，或运行`timer.py`以在每天7-8时随机时间自动运行填报脚本。
 
@@ -91,45 +68,13 @@ pip3 install requests selenium -i https://pypi.douban.com/simple --user
 "users": [
       {
          "username": "xxxxxxxxx", "password": "********",
-         "server_chan_key": "",
-         "temp_range": [36.0, 36.5], "reasons": [1, 1, 1, 1, 1, 1, 1],
-         "places": ["中心楼001", "中心楼001", "中心楼001", "中心楼001", "中心楼001", "中心楼001", "中心楼001"],
-         "email_addr": ""
+         "server_chan_key": "", "email_addr": "",
+         "temp_range": [36.0, 36.5]
       },
       {
          "username": "xxxxxxxxx", "password": "********",
-         "server_chan_key": "",
-         "temp_range": [36.0, 36.5], "reasons": [1, 1, 1, 1, 1, 1, 1],
-         "places": ["中心楼001", "中心楼001", "中心楼001", "中心楼001", "中心楼001", "中心楼001", "中心楼001"],
-         "email_addr": ""
+         "server_chan_key": "", "email_addr": "",
+         "temp_range": [36.0, 36.5]
       }
    ]
 ```
-
-## 5. 进阶
-
-本脚本现已支在docker环境中运行。此处简单介绍如何在未安装图形界面的服务器上，使用docker安装支持桌面环境的镜像，并实现每日自动签到。
-
-首先，使用docker拉下 [dorowu/ubuntu-desktop-lxde-vnc](https://hub.docker.com/r/dorowu/ubuntu-desktop-lxde-vnc/) 镜像。
-
-```shell
-docker pull dorowu/ubuntu-desktop-lxde-vnc:focal-lxqt 
-```
-
-然后，启动该容器并允许在`5900`端口上的VNC远程登录。其中，`VNC_PASSWORD`字段是VNC远程登录的密码，可以根据需要自行设置。
-
-```shell
-docker run -itd --name ubuntu-daily-report -p 5900:5900 \
-    -e RESOLUTION=1600x900 -e VNC_PASSWORD=123456 \
-    dorowu/ubuntu-desktop-lxde-vnc:focal-lxqt 
-```
-
-容器成功启动后，使用VNC远程连接容器内的操作系统，在操作系统内使用git拉下本仓库代码，并配置`config.json`。
-
-最后，使用图形桌面的终端运行`timer.py`脚本即可。
-
-```shell
-python3 timer.py
-```
-
-![](img/docker-with-desktop.jpg)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/bin/env python3
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
 import os
 import json
 import random
@@ -184,16 +186,16 @@ def daily_report(drv, user):
     wait_element_by_class_name(drv, 'mint-loadmore-top', 30)  # 等待界面加载 超时30s
     time.sleep(1)
     add_btn = drv.find_element('xpath', '//*[@id="app"]/div/div[1]/button[1]')  # 找到新增按钮
-    if add_btn.text == '退出':  # 没有找到新增按钮
-        message(user, '用户' + user['username'] + '今日已进行过每日上报！', '')
-        return
-    else:
+    if add_btn.text == '新增':
         add_btn.click()  # 点击新增填报按钮
         time.sleep(1)
         popup = find_element_by_class_keyword(drv, 'mint-msgbox-confirm', '确定')  # 查询是否弹出了对话框
         if popup is not None:  # 如果弹出了对话框
             message(user, '用户' + user['username'] + '当前不在每日健康上报时间!', '')
             return
+    else:  # 没有找到新增按钮
+        message(user, '用户' + user['username'] + '今日已进行过每日上报！', '')
+        return
 
     wait_element_by_class_name(drv, "emapm-form", 30)  # 等待界面动画
 
@@ -215,227 +217,36 @@ def daily_report(drv, user):
     message(user, '用户' + user['username'] + '每日上报成功', '')
 
 
-def select_default_item_by_keyword(drv, keyword):
+def get_driver():
     """
-    在入校申请时选择默认项
-
-    Args:
-        drv: webdriver对象
-        keyword: 关键词
-    """
-    items = drv.find_elements('class name', 'emapm-item')  # 找到所有项目
-    for item in items:
-        if item.text.find(keyword) >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直到元素可见
-            item.click()
-
-    wait_element_by_class_name(drv, 'mint-picker__confirm', 5)  # 等待弹出动画
-    time.sleep(1)
-    find_element_by_class_keyword(drv, 'mint-picker__confirm', '确定').click()  # 点击确定
-    time.sleep(1)
-
-
-def select_default_item_in_areas(drv, keyword):
-    """
-    在入校申请时选择通行区域
-
-    Args:
-        drv: webdriver对象
-        keyword: 通行区域关键词
-    """
-    items = drv.find_elements('class name', 'emapm-item')  # 找到所有项目
-    for item in items:
-        if item.text.find(keyword) >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直元素可见
-            item.click()
-
-    wait_element_by_class_name(drv, 'mint-checkbox-new-row', 5)  # 等待弹出动画
-    time.sleep(1)
-    drv.find_element('class name', 'mint-checkbox-new-row').click()  # 点击复选框
-    time.sleep(1)
-    find_element_by_class_keyword(drv, 'mint-selected-footer-confirm', '确定').click()  # 点击确定按钮
-    time.sleep(1)
-
-
-def picker_click(drv, column, cnt):
-    """
-    选择滚轮的中的项目
-
-    Args:
-        drv: webdriver对象
-        column: 滚轮的栏目
-        cnt: 要选择的位置
-    """
-    pickers = column.find_elements('class name', 'mt-picker-column-item')  # 所有滚动元素
-    drv.execute_script("arguments[0].scrollIntoView();", pickers[cnt])  # 滚动页面直元素可见
-    pickers[cnt].click()  # 选中元素
-
-
-def time_date_reason_pick(drv, user):
-    """
-    选择通行时间及申请理由
-
-    Args:
-        drv: webdriver对象
-        user: 用户信息
-    """
-    items = drv.find_elements('class name', 'emapm-item')  # 找到所有项目
-    for item in items:
-        if item.text.find('通行开始时间') >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直元素可见
-            item.click()  # 点击项目
-            columns = item.find_elements('class name', 'mint-picker-column')  # 找到项目内所有滚轮
-            time.sleep(1)
-            picker_click(drv, columns[0], date_of_tomorrow.date().year - 1920)  # 年 从1920年开始
-            picker_click(drv, columns[1], date_of_tomorrow.date().month - 1)  # 月 从1开始
-            picker_click(drv, columns[2], date_of_tomorrow.date().day - 1)  # 日 从1开始
-            picker_click(drv, columns[3], 7)  # 时
-            picker_click(drv, columns[4], 31)  # 分 入校时间为7时31分
-            time.sleep(1)
-            find_element_by_class_keyword(drv, 'mint-picker__confirm', '确定').click()  # 点击确定按钮
-            time.sleep(1)
-
-        if item.text.find('通行结束时间') >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直元素可见
-            item.click()  # 点击项目
-            columns = item.find_elements('class name', 'mint-picker-column')  # 找到项目内所有滚轮
-            time.sleep(1)
-            picker_click(drv, columns[0], date_of_tomorrow.date().year - 1920)  # 年 从1920年开始
-            picker_click(drv, columns[1], date_of_tomorrow.date().month - 1)  # 月 从1开始
-            picker_click(drv, columns[2], date_of_tomorrow.date().day - 1)  # 日 从1开始
-            picker_click(drv, columns[3], 21)  # 时
-            picker_click(drv, columns[4], 59)  # 分 出校时间为21时59分
-            time.sleep(1)
-            find_element_by_class_keyword(drv, 'mint-picker__confirm', '确定').click()  # 点击确定按钮
-            time.sleep(1)
-
-        if item.text.find('申请理由') >= 0:  # 找到项目标题
-            drv.execute_script("arguments[0].scrollIntoView();", item)  # 滚动页面直元素可见
-            item.click()  # 点击项目
-            column = item.find_element_by_class_name('mint-picker-column')  # 找到项目内所有滚轮
-            time.sleep(1)
-            picker_click(drv, column, user['reasons'][date_of_tomorrow.date().weekday()])  # 根据星期自动填写目的
-            time.sleep(1)
-            find_element_by_class_keyword(drv, 'mint-picker__confirm', '确定').click()  # 点击确定按钮
-            time.sleep(1)
-
-
-def check_today_report(drv):
-    """
-    检查当日是否已进行过入校申请
-
-    Args:
-        drv: webdriver对象
-    """
-    items = drv.find_elements('class name', 'res-list')  # 找到所有已填报项目
-    if len(items) == 0:
-        return False  # 第一次填报视为未填报
-    latest = find_element_by_class_keyword(items[0], 'res-item-ele', '申请时间').text  # 第一个项目即为最近一次的填报
-    latest = latest[latest.find(' ') + 1: latest.rfind(' ')]  # 只保留日期
-    latest_date = datetime.datetime.strptime(latest, '%Y-%m-%d').date()  # 转换
-
-    if latest_date == date_of_today.date():  # 今日已经填报过了
-        return True
-
-    return False
-
-
-def enter_campus_apply(drv, user):
-    """
-    进行入校申请
-
-    Args:
-        drv: webdriver对象
-        user: 用户信息
-    """
-    wait_element_by_class_name(drv, 'mint-loadmore-content', 30)  # 等待界面加载 超时30s
-
-    if check_today_report(drv):  # 今日已进行入校申请
-        message(user, '用户' + user['username'] + '今日已进行过入校申请！', '')
-        return
-
-    drv.find_element_by_class_name('mint-fixed-button').click()  # 找到并点击新增按钮
-
-    time.sleep(2)  # 等待窗口动画弹出
-    popup = find_element_by_class_keyword(drv, 'mint-msgbox-confirm', '确定')  # 查询是否弹出了对话框
-    if popup is not None:  # 如果弹出了对话框
-        message(user, '用户' + user['username'] + '当前不在入校申请填报时间!', '')
-        return
-
-    wait_element_by_class_name(drv, 'emapm-item', 30)  # 等待界面加载
-    select_default_item_by_keyword(drv, '身份证件类型')
-    select_default_item_by_keyword(drv, '工作场所是否符合防护要求')
-    select_default_item_by_keyword(drv, '工作人员能否做好个人防护')
-    select_default_item_by_keyword(drv, '是否已在南京居家隔离')
-    select_default_item_by_keyword(drv, '目前身体是否健康')
-
-    # 由于上传通信行程码的功能使用了微信内置浏览器API，普通浏览器暂时无法模拟，若您能够解决此问题，欢迎提出PR
-
-    select_default_item_in_areas(drv, '通行区域')  # 填写通行区域
-
-    time_date_reason_pick(drv, user)  # 填入入校时间/出校时间/入校理由
-
-    temp_input = find_element_by_class_placeholder_keyword(drv, 'mint-field-core', '请输入所到楼宇')
-    drv.execute_script("arguments[0].scrollIntoView();", temp_input)  # 滚动页面直元素可见
-    temp_input.click()  # 点击输入框
-    temp_input.send_keys(user['places'][date_of_tomorrow.weekday()])  # 输入入校地址
-
-    find_element_by_class_keyword(drv, 'tg-button', '提交').click()  # 点击提交按钮
-    wait_element_by_class_name(drv, 'mint-msgbox-confirm', 5)  # 等待弹出动画
-    time.sleep(1)
-    find_element_by_class_keyword(drv, 'mint-msgbox-confirm', '确定').click()  # 点击确认按钮
-
-    message(user, '用户' + user['username'] + '每日入校申请成功!', '')
-
-
-def get_driver(cfg):
-    """
-    根据平台及环境获取webdriver对象
-
-    Args:
-        cfg: 配置信息
+    获取webdriver对象
 
     Returns:
         drv: webdriver对象，若失败返回None
     """
-    # 判断平台
-    driver_option = webdriver.ChromeOptions()
     file_list = os.listdir()
 
-    os_platform = sys.platform
-    if os_platform == 'linux':
-        ret = os.popen('cat /proc/1/cgroup')
-        if ret.read().find('docker') != -1:  # 判断是否在docker中
-            print('\033[32m检测到Docker环境，禁用Chrome沙箱\033[0m')  # 检测到docker
-            driver_option.add_argument('--no-sandbox')  # 禁用浏览器沙箱
-
     # 判断浏览器类型
-    if cfg['browser'] == "chrome":
-        for file in file_list:
-            if file.lower().find('chromedriver') != -1:
-                return webdriver.Chrome(executable_path=os.path.join(current_folder, file), options=driver_option)
+    for file in file_list:
+        if file.lower().find('chromedriver') != -1:
+            return webdriver.Chrome(service=ChromeService(executable_path=os.path.join(current_folder, file)))
 
-    elif cfg['browser'] == "firefox":
-        for file in file_list:
-            if file.lower().find('geckodriver'):
-                return webdriver.Firefox(executable_path=os.path.join(current_folder, file))
-    else:
-        print('\033[31m请在config.json中指定正确的浏览器类型\033[0m')
-        return None
+    for file in file_list:
+        if file.lower().find('geckodriver') != -1:
+            return webdriver.Firefox(service=FirefoxService(executable_path=os.path.join(current_folder, file)))
 
     print("\033[31m找不到可用的Webdriver\033[0m")
     return None
 
 
-def run(user, config):
+def run(user):
     """
     填报一个用户
 
     Args:
         user: 要填报的用户
-        config: 配置信息
     """
-    driver = get_driver(config)
+    driver = get_driver()
     if driver is None:
         return
 
@@ -445,11 +256,6 @@ def run(user, config):
         login(driver, user)
         # 每日打卡
         daily_report(driver, user)
-        if config['enter_campus_apply']:
-            # 打开入校申请页面
-            driver.get(enter_campus_apply_url)
-            # 入校申请
-            enter_campus_apply(driver, user)
     except Exception:
         exception = traceback.format_exc()
         message(user, '用户'+user['username']+'每日上报过程中出错，请尝试手动重新填报', exception)
@@ -465,11 +271,10 @@ def run_all():
     with open(os.path.join(current_folder, 'config.json'), encoding='UTF-8') as config_file:
         j = json.load(config_file)
         users = j['users']
-        config = j['config']
 
         for user in users:
             print('\033[35m用户' + user['username'] + '正在每日上报...\033[0m')
-            run(user, config)
+            run(user)
             time.sleep(3)
 
 
